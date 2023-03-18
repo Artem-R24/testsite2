a7530f6824ef96eff3094110b322935ee498f77c
Artem-R24 <68281843+Artem-R24@users.noreply.github.com>
2023-03-18 14:51:50 +0300

    Update tagcreator.php

--- tagcreator.php
+++ tagcreator.php
@@ -2,19 +2,18 @@
 require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
 CModule::IncludeModule('iblock');
 
-$Iblock=2;// Id инфоблока с продукцией
-$matCode='MATERIAL'; // Код своства "Материал"
-$manufCode='MANUFACTURER'; // Код своства "Производитель"
+const I_BLOCK=2;// Id инфоблока с продукцией
+const MAT_CODE='MATERIAL'; // Код своства "Материал"
+const MANUF_CODE='MANUFACTURER'; // Код своства "Производитель"
 
-$arSelect = Array("ID","IBLOCK_ID", "NAME","PROPERTY_*");
-$arFilter = Array("IBLOCK_ID"=>IntVal($Iblock));
+$arSelect = Array("ID","IBLOCK_ID", "NAME","PROPERTY_MATERIAL","PROPERTY_MANUFACTURER");
+$arFilter = Array("IBLOCK_ID"=>I_BLOCK);
 $res = CIBlockElement::GetList(Array(), $arFilter, false, false, $arSelect);
 while($ob = $res->GetNextElement()){ 
-   $arFields = $ob->GetFields();  
-   $arFields['PROPERTIES'] = $ob->GetProperties();
-   $strTags=$arFields['PROPERTIES'][$matCode]['VALUE'].', '.$arFields['PROPERTIES'][$manufCode]['VALUE'];
+   $arProps = $ob->GetProperties();
+   $strTags=$arProps[MAT_CODE]['VALUE'].', '.$arProps[MANUF_CODE]['VALUE'];
    $el = new CIBlockElement;
-   $result = $el->Update($arFields['ID'],Array('TAGS'=>$strTags));
+   $result = $el->Update($ob->GetFields()['ID'],Array('TAGS'=>$strTags));
 }
 echo "Done!";
 ?>
