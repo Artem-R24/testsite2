108700c2d64de2508ee7a8784c393e30b54759be
Artem-R24 <68281843+Artem-R24@users.noreply.github.com>
2023-03-30 13:37:04 +0300

    updated #4 (2nd time)

--- tagcreator.php
+++ tagcreator.php
@@ -6,19 +6,17 @@ const I_BLOCK=2;// Id инфоблока с продукцией
 const MAT_CODE='MATERIAL'; // Код своства "Материал"
 const MANUF_CODE='MANUFACTURER'; // Код своства "Производитель"
 
-$arSelect = Array("ID","IBLOCK_ID", "NAME","PROPERTY_MATERIAL","PROPERTY_MANUFACTURER");
+$arSelect = Array("ID","IBLOCK_ID", "NAME","PROPERTY_MATERIAL","PROPERTY_MANUFACTURER","TAGS);
 $arFilter = Array("IBLOCK_ID"=>I_BLOCK);
 $res = CIBlockElement::GetList(Array(), $arFilter, false, false, $arSelect);
-$arrTags=Array();
+
 while($ob = $res->GetNextElement()){ 
    $arProps = $ob->GetProperties();
    $strTags=$arProps[MAT_CODE]['VALUE'].', '.$arProps[MANUF_CODE]['VALUE'];
-   if(!(in_array($strTags, $arrTags)))
+   if(strcmp($ob->GetFields()['TAGS'],$strTags)!=0)
     {
-	   $arrTags[]=$strTags;
        $el = new CIBlockElement;
        $result = $el->Update($ob->GetFields()['ID'],Array('TAGS'=>$strTags));
-	   //test_dump($strTags);
     }
 }
 echo "Done!";
